ee560ac74d9a67bde348f13afac12173115399a0
Lucas Ergh <lucasergh@gmail.com>
2024-11-25 20:16:18 -0500

    got some stuff going

---
+++ Components/Pages/TicTacToe.razor.cs
@@ -0,0 +1,19 @@
+﻿namespace TicTacToeBlazor.Components.Pages
+{
+    public class TicTacToeBoard
+    {
+        string[,] board;
+        public TicTacToeBoard()
+        {
+            board = new string[3,3];
+            Enumerable.Repeat(' ', 9).ToArray();
+        }
+        public string getCell(int row, int col){
+            return board[row,col];
+        }
+        public void setCell(int row, int col){
+            board[row,col] = "X";
+        }
+    }
+}
+
